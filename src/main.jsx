50e2148cfad045e1a3729502d7f7c0dc07734aba
ThiagoIndalecio <thiiindalecio@gmail.com>
2024-06-03 07:51:51 -0300

    version6-front

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import  Home  from './routers/Home'
-import  Movie from './routers/Movie'
-import  MovieDetail from './routers/MovieDetail'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './routers/Home'
+import Movie from './routers/Movie'
+import MovieDetail from './routers/MovieDetail'
+import Error404 from './routers/Error404';
 import App from './App';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children:[
+    children: [
       {
         path: "/",
         element: <Home />
@@ -22,15 +23,22 @@ const router = createBrowserRouter([
       {
         path: "movie/:id",
         element: <MovieDetail />
+      },
+
+      {
+        path: "*",
+        element: <Error404 />
       }
-   
+
+
     ]
   },
+
 ]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>       
+  <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- src/routers/MovieDetail.jsx
+++ src/routers/MovieDetail.jsx
@@ -1,113 +1,105 @@
-import { React, useEffect, useState } from "react";
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
 import MovieFooter from "../components/movie-footer";
 import axios from "axios";
-import 'moment/dist/locale/pt-br'
-import moment from 'moment'
-import '../styles/movie-detail.css'
-
+import 'moment/dist/locale/pt-br';
+import moment from 'moment';
+import '../styles/movie-detail.css';
+import Eror404 from "./Error404";
 
 export default function MovieDetail() {
+    const params = useParams();
 
-    const params = useParams()
-
-
-    const [session, setSession] = useState([])
-    const [movie, setMovie] = useState([])
-    const [elenco, setElenco] = useState([])
-    const [uniqueDay, setUniqueDay] = useState([])
+    const [session, setSession] = useState([]);
+    const [movie, setMovie] = useState([]);
+    const [elenco, setElenco] = useState([]);
+    const [uniqueDay, setUniqueDay] = useState([]);
     const [selectedDate, setSelectedDate] = useState([]);
-    const [sessionByDay, setSessionsByDay] = useState([])
-    
+    const [sessionByDay, setSessionsByDay] = useState([]);
+    const [selectedButtonId, setSelectedButtonId] = useState(null);
+    const [selectedHourButtonId, setSelectedHourButtonId] = useState(null);
 
-
-    const handleButtonClick =   ( dateValue) => {
-        
+    const handleButtonClick = (dateValue, id) => {
         setSelectedDate(dateValue);
-        const hoursSessions =   session.filter((hour) => moment(hour.sessionStartTime).format("DD") === dateValue)
-        setSessionsByDay(hoursSessions)
-       
-      
+        setSelectedButtonId(id);
+        const hoursSessions = session.filter((hour) => moment(hour.sessionStartTime).format("DD") === dateValue);
+        setSessionsByDay(hoursSessions);
     };
 
+    const handleHourButtonClick = (id) => {
+        setSelectedHourButtonId(id);
+    };
 
     useEffect(() => {
-
         async function dataSession() {
-            const response = await axios.get(`http://localhost:8080/api/cinema/sessions/movies/${params.id}`)
-            const data = response.data.content
+            const response = await axios.get(`http://localhost:8080/api/cinema/sessions/movies/${params.id}`);
+            const data = response.data.content;
 
-            console.log(data)
-            moment.locale('pt-br')
+            moment.locale('pt-br');
 
-            setSession(data)
+            setSession(data);
             const uniqueSessions = data.reduce((acc, session) => {
                 const sessionDate = moment(session.sessionStartTime).format('YYYY-MM-DD');
                 if (!acc.find(item => moment(item.sessionStartTime).format('YYYY-MM-DD') === sessionDate)) {
                     acc.push(session);
                 }
                 return acc;
-            }, [])
+            }, []);
 
             uniqueSessions.sort((a, b) => {
                 return new Date(a.sessionStartTime) - new Date(b.sessionStartTime);
-        
             });
 
-            setUniqueDay(uniqueSessions)
-            
-
+            setUniqueDay(uniqueSessions);
         }
+
         async function dataMovie() {
-            const response = await axios.get(`http://localhost:8080/api/cinema/movies/${params.id}`)
-            const dataMovie = response.data
-            setMovie(dataMovie)
-         
-
-            setElenco(response.data.movieCast.split(','))
+            const response = await axios.get(`http://localhost:8080/api/cinema/movies/${params.id}`);
+            const dataMovie = response.data;
+            setMovie(dataMovie);
+            setElenco(response.data.movieCast.split(','));
         }
 
-        dataMovie()
-        dataSession()
+        dataMovie();
+        dataSession();
 
-    }, [])
+
+    }, [params.id]);
+
+    { if (params.id === 'undefined') return(<Eror404 />)}
 
     return (
+       
         <>
             <div className="container-movies">
-
                 <div className="background-banner">
-                    <img src="https://static.vecteezy.com/system/resources/thumbnails/005/720/479/small/banner-abstract-background-board-for-text-and-message-design-modern-free-vector.jpg" alt="" />
+                    <img src="https://storage.googleapis.com/portal-da-promo/L01_banner_promocaodroetker-cinemacombolodecaneca-20221656088449614.jpg" alt="" />
                 </div>
                 <div className="session-row">
                     <div className="session-row-image">
-
+                        <img src={`http://localhost:8080/api/cinema/movies/get-image/${params.id}`} />
                     </div>
                     <div className="session-row-days">
                         {
                             uniqueDay.map((sessionDay) => (
                                 <button
                                     key={sessionDay.id}
-                                    onClick={ () => handleButtonClick( moment(sessionDay.sessionStartTime).format("DD"))}
-                                    className="button-days"
-                                    >
-
-                                    <span className="button-days-subtitle" >{moment(sessionDay.sessionStartTime).format("ddd")}</span>
-                                    <span className="button-days-tittle"  >{moment(sessionDay.sessionStartTime).format("DD")}</span>
-                                    <span className="button-days-end" >{moment(sessionDay.sessionStartTime).format("MMM")}</span>
+                                    onClick={() => handleButtonClick(moment(sessionDay.sessionStartTime).format("DD"), sessionDay.id)}
+                                    className={`button-days ${selectedButtonId === sessionDay.id ? 'selected' : ''}`}
+                                >
+                                    <span className="button-days-subtitle">{moment(sessionDay.sessionStartTime).format("ddd")}</span>
+                                    <span className="button-days-tittle">{moment(sessionDay.sessionStartTime).format("DD")}</span>
+                                    <span className="button-days-end">{moment(sessionDay.sessionStartTime).format("MMM")}</span>
                                 </button>
                             ))
                         }
-
                     </div>
-
                 </div>
                 <div className="session-decription">
                     <div className="session-sinopsys">
                         <div>
                             <h2>{movie.category} - {movie.duration}</h2>
                             <p>{movie.synopsis}</p>
-
                         </div>
                         <div className="session-sinopsys-staff">
                             <h2>Elenco e Equipe</h2>
@@ -120,33 +112,28 @@ export default function MovieDetail() {
                             <div className="session-sinopsys-director">
                                 <h2>Elenco</h2>
                                 <div>
-                                    {
-                                        elenco.map((elenco) => {
-                                            return <p key={elenco}>{elenco}</p>
-                                        })
-                                    }
+                                    {elenco.map((elenco) => (
+                                        <p key={elenco}>{elenco}</p>
+                                    ))}
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="sessions-button-hours">
-                        {
-                            sessionByDay.map((schedules) => (
-                                <button
-                                    key={schedules.id}
-                                    className="button-hours">{moment(schedules.sessionStartTime).format("dddd DD MMMM, h:mm")}
-
-                                </button>
-                            ))
-                        }
+                        {sessionByDay.map((schedules) => (
+                            <button
+                                key={schedules.id}  
+                                onClick={() => handleHourButtonClick(schedules.id)}
+                                className={`button-hours ${selectedHourButtonId === schedules.id ? 'selected' : ''}`}
+                            >
+                                {moment(schedules.sessionStartTime).format("dddd DD MMMM, HH:mm")}
+                            </button>
+                        ))}
                         <button className="button-price">Comprar</button>
                     </div>
                 </div>
-
-                <MovieFooter topMovies={[]} />
+                <MovieFooter />
             </div>
-
         </>
-    )
+    );
 }
-
